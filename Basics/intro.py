2b25ea93585245374bfffd3d67320f583888be78
Manjushri A <manjushriashok06@gmail.com>
2026-02-14 20:58:53 +0530

    Create intro.py

---
+++ Basics/intro.py
@@ -0,0 +1,17 @@
+print("Hello World!! ")
+a= 10
+b=12
+sum= a+b
+print(f"Sum> {sum}")
+
+a=int(input("Enter the first number: "))
+b=int(input("Enter the Second number: "))
+print(f"Sum of {a} and {b} is: {a+b}")
+
+'''
+def add(x, y=2):
+    return x + y
+
+print(add(3))
+print(add(3, 5))
+'''
